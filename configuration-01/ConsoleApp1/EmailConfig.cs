07b868a3272155b6e7bd3e77353a7a5e48bffbe4
Jose A. Fernandez <jfernandez@desarrollosnea.com>
2025-05-06 18:29:57 -0300

    [configuration-01] Config de apps. Ejemplo en consola

---
+++ configuration-01/ConsoleApp1/EmailConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class EmailConfig
+    {
+        public string Server { get; set; }
+        public string Port { get; set; }
+        public string User { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ configuration-01/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using ConsoleApp1;
+using Microsoft.Extensions.Configuration;
+
+IConfiguration configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .Build();
+
+
+
+//var title = "Programacion III";
+var title = configuration["Title"];
+
+//Option 1
+//var emailSection = configuration.GetSection("Email");
+//var emailServer = emailSection["Server"];
+//var emailPort = emailSection["Port"];
+
+//Option 2
+//var emailServer = configuration["Email:Server"];
+//var emailPort = configuration["Email:Port"];
+
+//Option 3
+var emailConfig = configuration.GetSection("Email").Get<EmailConfig>();
+
+var emailServer = emailConfig.Server;
+var emailPort = emailConfig.Port;
+
+
+
+
+Console.WriteLine($"Hello, {title}! The email server is {emailServer} on port {emailPort}.");
