292480b7c7c5778b0ce348cfab57eb588853030f
Jose A. Fernandez <jfernandez@desarrollosnea.com>
2025-05-08 17:30:40 -0300

    [csharp-01]

---
+++ csharp-01/ClassLibrary1/PersonaBase.cs
@@ -0,0 +1,19 @@
+﻿namespace ClassLibrary1
+{
+    public class PersonaBase
+    {
+        private DateTime _fechaNacimiento;
+
+        public int Edad { get; set; }
+
+        public DateTime FechaNacimiento
+        {
+            get { return _fechaNacimiento; }
+            set
+            {
+                _fechaNacimiento = value;
+                Edad = DateTime.Now.Year - _fechaNacimiento.Year;
+            }
+        }
+    }
+}
---
+++ csharp-01/TestProject1/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using ClassLibrary1;
+
+namespace TestProject1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var alumno = new Alumno();
+            alumno.FechaNacimiento = new DateTime(2000, 1, 1);
+
+            Assert.True(alumno.Edad == 25, "La edad no es correcta.");
+
+
+        }
+    }
+}
